1670788f11d8090a0e8876aeeb2b701f807e3b7a
5910501950 <somapa.sakara@gmail.com>
2017-09-08 11:52:25 +0700

    add move herizontal and change angle

--- models.py
+++ models.py
@@ -1,13 +1,33 @@
+import arcade.key
+
 class Ship:
+    DIR_HORIZONTAL = 0
+    DIR_VERTICAL = 1
+
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+        self.direction = Ship.DIR_VERTICAL
+        self.angle = 0
  
     def update(self, delta):
-        if self.y > self.world.height:
-            self.y = 0
-        self.y += 5
+        if self.direction == Ship.DIR_VERTICAL:    
+            if self.y > self.world.height:
+                self.y = 0
+            self.y += 5
+            self.angle = 0
+        else:
+            if self.x > self.world.width:
+                self.x = 0
+            self.x += 5
+            self.angle = -90
+
+    def switch_direction(self):
+        if self.direction == Ship.DIR_HORIZONTAL:
+            self.direction = Ship.DIR_VERTICAL
+        else:
+            self.direction = Ship.DIR_HORIZONTAL
 
 class World:
     def __init__(self, width, height):
@@ -16,6 +36,9 @@ class World:
  
         self.ship = Ship(self, 100, 100)
  
- 
     def update(self, delta):
         self.ship.update(delta)
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.SPACE:
+            self.ship.switch_direction()
--- space.py
+++ space.py
@@ -4,16 +4,31 @@ from models import *
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
  
+        super().__init__(*args, **kwargs)
+ 
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+ 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
  
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.ship_sprite = arcade.Sprite('images/ship.png')
-
         self.world = World(width, height)
+
+        self.ship_sprite = ModelSprite('images/ship.png',model=self.world.ship)
  
     def on_draw(self):
         arcade.start_render()
@@ -21,9 +36,11 @@ class SpaceGameWindow(arcade.Window):
 
     def update(self, delta):
         self.world.update(delta)
-        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
+        #self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
+    
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
- 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
