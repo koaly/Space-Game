adcc0121ac46b4a786e80bc22ebb055b3a175ea1
5910501950 <somapa.sakara@gmail.com>
2017-09-08 11:25:46 +0700

    add create and update ship to world

--- space.py
+++ space.py
@@ -1,6 +1,6 @@
 import arcade
 
-from models import Ship
+from models import *
  
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 600
@@ -11,18 +11,17 @@ class SpaceGameWindow(arcade.Window):
  
         arcade.set_background_color(arcade.color.BLACK)
 
-        self.ship = Ship(100,100)
         self.ship_sprite = arcade.Sprite('images/ship.png')
+
+        self.world = World(width, height)
  
     def on_draw(self):
         arcade.start_render()
         self.ship_sprite.draw()
 
     def update(self, delta):
-        ship = self.ship
- 
-        ship.update(delta)
-        self.ship_sprite.set_position(ship.x, ship.y)
+        self.world.update(delta)
+        self.ship_sprite.set_position(self.world.ship.x, self.world.ship.y)
 
  
 if __name__ == '__main__':
